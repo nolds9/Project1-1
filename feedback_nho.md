eeac4b07bb5dc437d0fbb04a962edfa3e2d1b012
Nicholas Olds <nolds9@yahoo.com>
2015-11-04 15:46:14 -0500

    NHO: add feedback for NT project 1

---
+++ feedback_nho.md
@@ -0,0 +1,31 @@
+# Project Feedback
+
+## Project Workflow
+
+>Did you complete the user stories, wireframes, task tracking, and/or ERDs, as specified above? Did you use source control as expected for the phase of the program you’re in (detailed above)?
+
+**Meets Expectations**. Nice work coming up with a working project in 3 days! Would like to see more specific user stories from the perspective of someone playing the game, using the app, etc. As we move forward for larger projects, it would be nice to have more thoughts from your planning process, such as a Trello board, and relevant wireframes.
+
+## Technical Requirements
+
+>Did you deliver a project that met all the technical requirements? Given what the class has covered so far, did you build something that was reasonably complex?
+
+**Meets Expectations**. Congratulations, your project shows prolific use of the technologies and concepts we have covered so far! I think there is some easily identifiable low-hanging fruit that you could knock-out to add the finishing touches to increase functionality.
+
+## Code Quality
+
+>Did you follow code style guidance and best practices covered in class, such as spacing, modularity, and semantic naming? Did you comment your code as your instructors have in class?
+
+**Meets Expectations**. Html is clearly structured and validated, CSS looks good, and JS works! Moving forward, try to think about ways to simplify your code, and provide effective wrappers for your data. Would recommend trying an object-oriented approach in the future. In the future, please remove unused/commented out code
+
+ Please review my [inline code comments here](https://github.com/shindigira/Project1/compare/master...nolds9:feedback) (prefixed with my initials - "NHO").
+
+
+## Problem Solving
+
+>Are you able to defend why you implemented your solution in a certain way? Can you demonstrate that you thought through alternative implementations?
+
+**Meets Expectations**
+I like the approach you took to break down the logic of the game into functions with a clear separation of concerns. In general, I'd try to think of ways to simplify and reduce repetition.
+
+Great job!
--- script.js
+++ script.js
@@ -1,7 +1,5 @@
+// NHO: Document.ready function should wrap around all javascript
 $(document).ready(function(){
-});
-
-
   var firstImage = "http://usercontent2.hubimg.com/6089769_f260.jpg";
   var count = 0;
   var cardList = ["#one .front .img","#two .front .img","#three .front .img","#four .front .img","#five .front .img","#six .front .img","#seven .front .img","#eight .front .img"];
@@ -17,7 +15,7 @@ $(document).ready(function(){
   ];
 
   /*Random Image Generator*/
-
+  // NHO: really like this function!
   function returnRandomSelected(){
     var order = [];
     var count = 0;
@@ -28,7 +26,7 @@ $(document).ready(function(){
      var randomlyselectedcard = Math.floor(randomnumber*cards);
      return randomlyselectedcard;
     }
-    while(count < 8){
+    while(count < 8){ // NHO: how could we reference a value rather than hard-code the data, say if we wanted to play with more than 8 cards
       var a = randomSelected();
       if(!mySet.has(a)){
           mySet.add(a);
@@ -42,14 +40,14 @@ $(document).ready(function(){
   /***********************/
   var imageArray = [];
 
-  function listeners() {
+  function listeners() { // NHO: How could we simplify these event listeners?
       $("#one").on("click", function(e) {
           if($(e.target).attr('src')===firstImage){
             count++;
           }
           $(e.target).attr('src', images[cOrder[0]]);
           imageArray.push(images[cOrder[0]]);
-          // count++;
+          // count++; // NHO: Remember to remove unused / commented out code
           if(count === 2){
             countClicks();
           }
@@ -132,8 +130,7 @@ $(document).ready(function(){
             countClicks();
           }
       });
-
-  }
+  } // NHO: would like to think about ways to make this event more DRY to avoid hard-codeing index values
 
   function countClicks(){
       //console.log(imageArray);
@@ -144,15 +141,15 @@ $(document).ready(function(){
 
   }
 
-  function checkTwo(){//check for same
+  function checkTwo(){//check for same // NHO: Nice comments!
     for(var x = 0; x < cardList.length; x++){
       for(var y = 0; y < cardList.length; y++){
         if (x!==y){//check every other card except itself
           if($(cardList[x]).attr('src')===$(cardList[y]).attr('src') && $(cardList[x]).attr('src') !== firstImage && $(cardList[y]).attr('src') !== firstImage){
             console.log("Match!");
             alert("Match!");
-            cOrder = returnRandomSelected();
-            return;
+            cOrder = returnRandomSelected(); // NHO: we randomize the board after every match?
+            return; // NHO: what are we returning?
           }
         }
       }
@@ -166,49 +163,8 @@ $(document).ready(function(){
 
   listeners();
 
+}); // NHO: close document.ready
+// NHO: Make sure to remove commented out code!
 
-
-// var cardList = ["#one","#two"];
-// var images = [
-//   "https://s3.amazonaws.com/rapgenius/bboy11.jpg",
-//   "https://farm1.staticflickr.com/389/20298537230_cdce4787ee_o_d.jpg",
-//   "http://www.photoshopytutoriales.com/wp-content/uploads/2011/12/tutorial_breakdance_fotomanipulacion.jpg",
-//   "http://farm2.static.flickr.com/1281/4660602801_3065267f18.jpg"
-// ]
-//
-// function listeners(){
-//   $("#one").on("click", function(e) {
-//     var self = this;
-//     console.log(self);
-//         $(e.target).attr('src', images[0]);
-//     });
-//   $("#two").on("click", function(e) {
-//         $(e.target).attr('src',
-//           images[1]);
-//     });
-//   $("#three").on("click", function(e) {
-//         $(e.target).attr('src',            images[2]);
-//     });
-//   $("#four").on("click", function(e) {
-//         $(e.target).attr('src',            images[3]);
-//     });
-//   $("#five").on("click", function(e) {
-//         $(e.target).attr('src', "https://s3.amazonaws.com/rapgenius/bboy11.jpg");
-//     });
-//   $("#six").on("click", function(e) {
-//         $(e.target).attr('src',
-//           "https://farm1.staticflickr.com/389/20298537230_cdce4787ee_o_d.jpg");
-//     });
-//   $("#seven").on("click", function(e) {
-//         $(e.target).attr('src',            "http://www.photoshopytutoriales.com/wp-content/uploads/2011/12/tutorial_breakdance_fotomanipulacion.jpg");
-//     });
-//   $("#eight").on("click", function(e) {
-//         $(e.target).attr('src',            "http://farm2.static.flickr.com/1281/4660602801_3065267f18.jpg");
-//     });
-//
-//   checkTwo();
-// }
-//
-//
-//
-// listeners();
+// NHO: Overall, JS looks good. I think you have isolated so potential spots to come back to
+// and would love to see you knock-out some of the low-hanging fruit
